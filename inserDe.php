c7564066e3b7a007d597c7e78476e00602385f33
frankpezo <73926318+frankpezo@users.noreply.github.com>
2023-04-09 13:41:56 -0500

    Consulta para agregar pedido

---
+++ inserDe.php
@@ -0,0 +1,30 @@
+<?php
+//1. Traemos la conexión de la base de datos
+include('conexion.php');
+
+//2. Verificamos que los campos no estén vacíos
+if( /* !isset($_POST['idP'])   || */!isset($_POST['cantidad']) || !isset($_POST['monto'])){
+    exit();
+    
+}
+//2.1. Traemos los campos
+//$idP= $_POST['idP'];
+$cantidad = $_POST['cantidad'];
+$monto= $_POST['monto'];
+
+//3. Realizamos la consulta
+$sentencia = $conexion->prepare("INSERT INTO detalle_pedido(cantidad, monto) VALUES (?, ?)" );
+$resultado = $sentencia->execute([ $cantidad, $monto]);
+
+//4. Creamos el array 
+$arr = array();
+if($resultado === true){
+    $arr['success']= "true";
+}
+//4.1. Imprimimos
+print(json_encode($arr));
+
+
+
+
+?>
